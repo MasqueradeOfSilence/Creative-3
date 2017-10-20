bdd42588879c19ce613a2c8210754855d99aca8d
Andrew Neville <neville_andrew@yahoo.com>
2017-10-20 12:12:10 -0600

    added time sidebar

--- app.js
+++ app.js
@@ -35,6 +35,38 @@ angular.module('Schedule', ['ui.router'])
       "21:00",
       "21:30",
       "22:00"
+    ],
+
+    timeStrings2: ["7:00",
+      "7:30",
+      "8:00",
+      "8:30",
+      "9:00",
+      "9:30",
+      "10:00",
+      "10:30",
+      "11:00",
+      "11:30",
+      "12:00",
+      "12:30",
+      "13:00",
+      "13:30",
+      "14:00",
+      "14:30",
+      "15:00",
+      "15:30",
+      "16:00",
+      "16:30",
+      "17:00",
+      "17:30",
+      "18:00",
+      "18:30",
+      "19:00",
+      "19:30",
+      "20:00",
+      "20:30",
+      "21:00",
+      "21:30"
     ]
   };
   return o;
@@ -55,6 +87,7 @@ angular.module('Schedule', ['ui.router'])
     console.log($scope.table);
     $scope.daysToAdd = tableFactory.daysToAdd;
     $scope.timeStrings = tableFactory.timeStrings;
+    $scope.timeStrings2 = tableFactory.timeStrings2;
     //$scope.test = 'Hello world!';
 
     /*  $scope.posts = [
