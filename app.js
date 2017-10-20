cf9a652e92e87edba47e62873019f254fcca771c
Andrew Neville <neville_andrew@yahoo.com>
2017-10-19 23:53:22 -0600

    working array, start of table

--- app.js
+++ app.js
@@ -16,25 +16,25 @@ angular.module('Schedule', ['ui.router'])
       "11:30",
       "12:00",
       "12:30",
-      "1:00",
-      "1:30",
-      "2:00",
-      "2:30",
-      "3:00",
-      "3:30",
-      "4:00",
-      "4:30",
-      "5:00",
-      "5:30",
-      "6:00",
-      "6:30",
-      "7:00",
-      "7:30",
-      "8:00",
-      "8:30",
-      "9:00",
-      "9:30",
-      "10:00"
+      "13:00",
+      "13:30",
+      "14:00",
+      "14:30",
+      "15:00",
+      "15:30",
+      "16:00",
+      "16:30",
+      "17:00",
+      "17:30",
+      "18:00",
+      "18:30",
+      "19:00",
+      "19:30",
+      "20:00",
+      "20:30",
+      "21:00",
+      "21:30",
+      "22:00"
     ]
   };
   return o;
@@ -44,6 +44,16 @@ angular.module('Schedule', ['ui.router'])
   'tableFactory',
   function($scope, tableFactory){
     $scope.table = tableFactory.table;
+    for (var i = 0; i < 5; i++) {
+      $scope.table[i] = [30];
+    }
+    for (var i = 0; i < 5; i++) {
+      for (var j = 0; j < 30; j++) {
+        $scope.table[i][j] = [];
+      }
+    }
+    console.log($scope.table);
+    $scope.daysToAdd = tableFactory.daysToAdd;
     $scope.timeStrings = tableFactory.timeStrings;
     //$scope.test = 'Hello world!';
 
@@ -58,21 +68,21 @@ angular.module('Schedule', ['ui.router'])
   $scope.addClass = function(){
     if($scope.formContent === '') { return; }
 
-    daysToAdd[0] = $scope.monday;
-    daysToAdd[1] = $scope.tuesday;
-    daysToAdd[2] = $scope.wednesday;
-    daysToAdd[3] = $scope.thursday;
-    daysToAdd[4] = $scope.friday;
+    $scope.daysToAdd[0] = $scope.monday;
+    $scope.daysToAdd[1] = $scope.tuesday;
+    $scope.daysToAdd[2] = $scope.wednesday;
+    $scope.daysToAdd[3] = $scope.thursday;
+    $scope.daysToAdd[4] = $scope.friday;
 
-    for (var i = 0; i < daysToAdd.length; i++) {
-      if(daysToAdd[i] == true) {
+    for (var i = 0; i < $scope.daysToAdd.length; i++) {
+      if($scope.daysToAdd[i] == true) {
         var startIndex = 0;
         var endIndex = 29;
-        for (var t = 0; t < timeStrings.length; t++) {
-          if (timeStrings[t] == $scope.startTime) {
+        for (var t = 0; t < $scope.timeStrings.length; t++) {
+          if ($scope.timeStrings[t] == $scope.startTime) {
             startIndex = t;
           }
-          if (timeStrings[t] == $scope.endTime) {
+          if ($scope.timeStrings[t] == $scope.endTime) {
             endIndex = t;
           }
         }
@@ -80,14 +90,17 @@ angular.module('Schedule', ['ui.router'])
 
 
         $scope.table[i][startIndex].push({
+        //$scope.table[1][1].push({
           title: $scope.formContent
         });
+        //$scope.table[i][startIndex].push($scope.formContent);
+        //$scope.table.push($scope.formContent);
         for (var remainingCells = startIndex + 1; remainingCells < endIndex; remainingCells++) {
           $scope.table[i][remainingCells].push({});
         }
       }
     }
-
+    console.log($scope.table);
 
 /*
     $scope.table.push({
